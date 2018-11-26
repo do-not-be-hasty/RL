7fa4ec828ac057f390ff02c5ea7ba7e7a0299121
Michał <michal.zawalski25@gmail.com>
2018-11-26 20:53:04 +0100

    DQN with GoalEnv interface

--- src/run_her.py
+++ src/run_her.py
@@ -1,18 +1,15 @@
 import gym
-import numpy as np
-import sys
 from gym_maze.envs import MazeEnv
-from stable_baselines.bench import Monitor
 
 from stable_baselines.common.policies import MlpPolicy as PPO2_Policy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import PPO2, DQN, DDPG
 from stable_baselines.deepq import MlpPolicy as DQN_Policy
 from stable_baselines.ddpg import MlpPolicy as DDPG_Policy
-from stable_baselines.her import HER
-from stable_baselines.results_plotter import ts2xy, load_results
 
 from utility import resources_dir, get_cur_time_str
+
+from DQN_HER import DQN_HER as HER
 
 
 n_steps = 0
@@ -29,6 +26,13 @@ def callback(_locals, _globals):
         return False
 
     is_printed = False
+
+    try:
+        # For pure gym env, eg DQN_HER
+        _locals['self'].get_env().print_rewards_info()
+        is_printed = True
+    except AttributeError:
+        pass
 
     try:
         # For DummyVecEnv, eg PPO2
@@ -53,11 +57,11 @@ def callback(_locals, _globals):
 
 
 def main():
-    env = gym.make('MazeEnv-v0')
-    env = DummyVecEnv([lambda: env])
+    env = gym.make('GoalMazeEnv-v0')
+    # env = DummyVecEnv([lambda: env])
 
-    model = PPO2(
-        policy=PPO2_Policy,
+    model = HER(
+        policy=DQN_Policy,
         env=env,
         learning_rate=1e-3,
         # buffer_size=50000,
@@ -66,24 +70,27 @@ def main():
         # verbose=1,
     )
 
-    model = model.learn(total_timesteps=100000, callback=callback)
+    model = model.learn(total_timesteps=50000, callback=callback)
     model.save(str(resources_dir().joinpath('model.pkl')))
 
-    env.env_method('_set_live_display', True)
-    env.env_method('_set_step_limit', 100)
-    env.env_method('reset_rewards_info')
+    env._set_live_display(True)
+    env._set_step_limit(100)
+    env.reset_rewards_info()
 
     print('--- Evaluation\n')
 
-    obs = env.reset()
+    obs_dict = env.reset()
+    obs = obs_dict['observation']
     for i in range(1000):
         action, _states = model.predict(obs)
-        obs, rewards, dones, info = env.step(action)
+        obs_dict, rewards, dones, info = env.step(action)
+        obs = obs_dict['observation']
         env.render()
 
         if dones:
-            obs = env.reset()
-            env.env_method('print_rewards_info')
+            obs_dict = env.reset()
+            obs = obs_dict['observation']
+            env.print_rewards_info()
             print()
 
 if __name__ == '__main__':
